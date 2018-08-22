4f30b26151b8ce87f69790fafa7e82bc1d86a0e9
Connie <connie2630a@gmail.com>
2018-08-22 13:22:44 -0500

    added CLI.rb file

---
+++ spec/best_cars_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe BestCars do
+  it "has a version number" do
+    expect(BestCars::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
