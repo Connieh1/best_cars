a500d72452269ecfb8db1288da4b4adb2ff17b6d
Connie <connie2630a@gmail.com>
2018-08-22 17:34:51 -0500

    add title to cli

--- lib/best_cars/cli.rb
+++ lib/best_cars/cli.rb
@@ -1,12 +1,30 @@
+require "open-uri"
+require "nokogiri"
+
+
+
 class BestCars::CLI
 
+	
+
+
 	def call
-		puts "10 Best Cars:"
-		list_cars
+		puts title
+		# puts "10 Best Cars:"
+		# list_cars
+	end
+
+	def title
+		html = open('https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature')
+		doc = Nokogiri::HTML(html)
+		doc.css(".mv1").text
 	end
 
 	def list_cars
 		#scrapes car & driver's list
+		# url = https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature
+		# html = open('url')
+		# doc = Nokogiri::HTML(html)
 	end
 
 end
