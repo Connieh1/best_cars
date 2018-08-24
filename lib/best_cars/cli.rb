1619ad7698ed7e07c059c7348f8a3042d37e9b13
Connie <connie2630a@gmail.com>
2018-08-24 00:06:19 -0500

    scraped cars

--- lib/best_cars/cli.rb
+++ lib/best_cars/cli.rb
@@ -3,83 +3,76 @@ class BestCars::CLI
 
 
 	def call
-		# puts title
-		
-
-# 		# puts "\n"
+		puts "\n"
+		puts title
+		puts "\n"
 		list_cars
-		# menu
-# 		# goodbye
-
+		menu
 	end
 
-	# def title
-	# 	html = open('https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature')
-	# 	doc = Nokogiri::HTML(html)
-	# 	doc.css(".mv1").text 
-	# end
+	def title
+		html = open('https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature')
+		doc = Nokogiri::HTML(html)
+		doc.css(".mv1").text 
+	end
 
 	def list_cars
-		puts "10 Best Cars:"
 		@cars = BestCars::Car.carList
 		@cars.each.with_index(1) do |car, i|
-			puts "#{i}. #{car.name}"
-		# puts <<-DOC
-		# 1. car_1 aoc
-		# 2. car_2 boc
-		# DOC
-		# html = open('https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature')
-		# doc = Nokogiri::HTML(html)
-		# # doc.css(".small-12").css("span").each do |car| puts car.text end
-		# # doc.css(".small-12").first.css("span").text
-		# # 2 doc.css(".small-12")[1].css("span").text
-		# puts title
-		# doc.css(".small-12").css("span").each do |car| puts car.text end
-		# @cars = BestCars::Car.carList
+		  puts "#{i}. #{car.name}"
 		end
 	end
 
 
 	def menu
-		# puts "\n"
-		# puts "Enter the number of the car you would like more information on"
+		puts "\n"
 		input = nil
 		until input == "exit"
-		puts "Please enter the number of car you would like more information about, or exit"
-		# "Enter a command : \n I accept:\n 1 to 10 \n list \n exit"
+		puts "Please enter the number of car you would like more information about (1-10), list, or exit"
 		  input = gets.strip.downcase
 		  case input
 		  when "1"
-			puts "More info on Car 1"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_1.comp
 		  when "2"
-			puts "More info on Car 2"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_2.comp
 		  when "3"
-			puts "More info on Car 3"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_3.comp
 		  when "4"
-			puts "More info on Car 4"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_4.comp
 		  when "5"
-			puts "More info on Car 5"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_5.comp
 		  when   "6"
-		  	puts "More info on Car 6"
+		  	puts "\n"
+		    puts BestCars::Car.scrape_car_6.comp
 		  when "7"
-			puts "More info on Car 7"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_7.comp
 		  when "8"
-			puts "More info on Car 8"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_8.comp
 		  when "9"
-			puts "More info on Car 9"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_9.comp
 		  when "10"
-			puts "More info on Car 10"
+		  	puts "\n"
+			puts BestCars::Car.scrape_car_10.comp
 		  when "exit"
-			puts puts "See you next time!"
-		  when input !=(1..10)
-		  	puts "Please enter an number between 1 and 10"
+		  	puts "\n"
+			puts "See you next time!"
+			puts "\n"
+		  when "list"
+		  	puts "\n"
+		  	list_cars
+		  	puts "\n"
 		  end
 		end
 	end
 
-# 	# def goodbye
-# 	#   puts "See you next time!"
-# 	# end
 
 end
 
