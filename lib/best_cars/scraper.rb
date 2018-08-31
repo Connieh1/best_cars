86398f19714e576e81768debbd9c2c7655d22840
Connie <connie2630a@gmail.com>
2018-08-30 21:25:05 -0500

    new scraper methods

--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -1,12 +1,21 @@
 class BestCars::Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature"))
+
+  def index_page
+  	Nokogiri::HTML(open("https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature"))
   end
 
-  def scraped_cars
-    get_page.css(".small-12")
+  def scrape_cars
+  	self.index_page.css(".tenbest2018").css("a")
   end
+
+  # def get_page
+  #   Nokogiri::HTML(open("https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature"))
+  # end
+
+  # def scraped_cars
+  #   get_page.css(".small-12")
+  # end
 
   
 end
