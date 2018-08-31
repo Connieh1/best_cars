ff6633824920b1ce4bef095f9f34b13281faf8c3
Connie <connie2630a@gmail.com>
2018-08-30 21:55:01 -0500

    update scraper

--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -9,12 +9,17 @@ class BestCars::Scraper
   	self.index_page.css(".tenbest2018").css("a")
   end
 
-  def car_sites
-  	self.scrape_cars.each do |car| 
-  	  "#{car.attribute("href")}" 
-  	end 
-  end
+  # def car_sites
+  # 	self.scrape_cars.each do |car| 
+  # 	  "#{car.attribute("href")}" 
+  # 	end 
+  # end
 
+  def make_cars
+  	scrape_cars.each do |cars|
+  	  BestCars::Car.new_from_index_page(r)
+  	end
+  end
   # def get_page
   #   Nokogiri::HTML(open("https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature"))
   # end
