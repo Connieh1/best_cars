1d11e8a8bf02ec2f438188b637c40e92b1dc6510
Connie <connie2630a@gmail.com>
2018-08-30 21:34:14 -0500

    built car_sites method

--- lib/best_cars/scraper.rb
+++ lib/best_cars/scraper.rb
@@ -9,6 +9,12 @@ class BestCars::Scraper
   	self.index_page.css(".tenbest2018").css("a")
   end
 
+  def car_sites
+  	self.scrape_cars.each do |car| 
+  	  "#{car.attribute("href")}" 
+  	end 
+  end
+
   # def get_page
   #   Nokogiri::HTML(open("https://www.caranddriver.com/features/2018-10best-cars-the-best-cars-for-sale-in-america-today-feature"))
   # end
